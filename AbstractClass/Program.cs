5b1c87518f7dfbd4da413265dbbe182ce1d88c30
Lương IT <songoku60@outlook.com.vn>
2023-11-27 13:03:23 +0700

    27/11

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using AbstractClass;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+//object initializer => giam thieu constructor
+Teacher teacher = new()
+{
+    FullName = "luong",
+    Age = 46,
+};
+
+teacher.ShowInfo();
+Console.WriteLine(teacher);
+
